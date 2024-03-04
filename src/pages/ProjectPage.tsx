eafeb14fb3babedfa6a00a90a313f3d625b0e5ba
Mikaelia <mikaelagurney@gmail.com>
2024-03-03 20:00:26 -0800

    Update pot

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -8,8 +8,8 @@ const PotPage = ({
   title: string;
 }) => {
   return (
-    <div className="home mt-65 flex h-[calc(100vh-63px)] justify-start p-3 pr-0 ">
-      <div className="child-container h-full w-full max-w-5xl overflow-hidden  pr-3">
+    <div className="home mt-65 flex h-[calc(100vh-63px)] justify-start pb-3 pt-3">
+      <div className="child-container mr-3 h-full w-full max-w-5xl overflow-hidden rounded-br-md rounded-tr-md bg-gray4 p-3">
         {children}
       </div>
       <div className="sidePanel w-1/3 rounded-bl-md rounded-tl-md border border-r-0 border-gray5 bg-gray4 p-8 text-xl font-semibold">

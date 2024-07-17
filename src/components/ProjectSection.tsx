21db3bb56d52ad37dadaa57a16f6f03e7768c66d
Mikaelia <mikaelagurney@gmail.com>
2024-07-16 21:30:47 -0700

    Design tweaks

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -9,19 +9,22 @@ export const ProjectSection = () => {
 
   return (
     <div className="projects-container relative z-[-2] mt-[-20rem] flex w-screen flex-col items-center  justify-center bg-black pb-[30rem] pt-[30rem]">
-      <div className="flex w-full max-w-[2000px] flex-col justify-between md:flex-row">
-        <div className="text flex flex-col items-center justify-center md:ml-12 md:items-start">
-          <h2 id="projects" className="mb-4 font-display text-5xl font-normal">
-            PROJECTS
-          </h2>
-          <p>A collection of cool effects, animations, and mini-projects.</p>
+      <div className="container z-10 mt-14 flex flex-col items-center">
+        <div className="mb-14 flex w-full max-w-[2000px] flex-col justify-between md:flex-row">
+          <div className="text flex flex-col items-center justify-center md:mx-6">
+            <h2
+              id="projects"
+              className="mb-4 self-start font-display text-5xl font-normal"
+            >
+              PROJECTS
+            </h2>
+            <p>A collection of cool effects, animations, and mini-projects.</p>
+          </div>
+          <Filters
+            className="ml-auto mr-6 hidden md:block "
+            callback={(val: string) => setVisibleProjects(val)}
+          ></Filters>
         </div>
-        <Filters
-          className="hidden md:block"
-          callback={(val: string) => setVisibleProjects(val)}
-        ></Filters>
-      </div>
-      <div className="container z-10 mt-36 flex flex-col items-center">
         <ProjectList
           visibleProjects={visibleProjects}
           projBgRef={projBgRef}
